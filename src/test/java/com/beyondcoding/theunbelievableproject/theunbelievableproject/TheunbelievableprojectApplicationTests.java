177674d71399c91cfbdb535d4bd2b47962f80237
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-09-22 17:49:41 +0200

    Add unit test

--- src/test/java/com/beyondcoding/theunbelievableproject/theunbelievableproject/TheunbelievableprojectApplicationTests.java
+++ src/test/java/com/beyondcoding/theunbelievableproject/theunbelievableproject/TheunbelievableprojectApplicationTests.java
@@ -1,16 +1,43 @@
 package com.beyondcoding.theunbelievableproject.theunbelievableproject;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class TheunbelievableprojectApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .build();
+    }
+
+    @Test
+    public void contextLoads() throws Exception {
+        ResultActions result = mvc.perform(get("/"))
+                              .andExpect(status().is2xxSuccessful())
+                              .andExpect(content().string("The Unbelievable Project"));
+    }
 
 }
