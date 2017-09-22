abe12f7915768920781c8a7de774ad3a234b626a
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2017-09-22 17:06:47 +0200

    First stable version

---
+++ src/test/java/com/beyondcoding/theunbelievableproject/theunbelievableproject/TheunbelievableprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beyondcoding.theunbelievableproject.theunbelievableproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheunbelievableprojectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
